a2b311ee7325d6117a92f61470881c3d4fb620bc
kakeru0371 <pink.tiger7777@gmail.com>
2024-06-22 22:08:49 +0900

    create intro

--- guess.py
+++ guess.py
@@ -1,1 +1,17 @@
 import random
+
+def main():
+    #答えの生成
+    answer = random.randint(100, 999)
+
+    guess()
+
+def guess():
+    max_count = 10
+    for i in range(1, max_count + 1):
+        print('３桁の数字を入力してください')
+        num = int(input())
+
+
+if __name__ == '__main__':
+    main()
