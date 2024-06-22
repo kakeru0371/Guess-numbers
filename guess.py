37ffe3f3852836ea632e0c73df8d5cdd598e9ef9
kakeru0371 <pink.tiger7777@gmail.com>
2024-06-22 22:36:05 +0900

    create generator

--- guess.py
+++ guess.py
@@ -1,16 +1,29 @@
 import random
 
+#答えの生成
+def generator():
+    answer = []
+    while len(answer) < 3:
+        rand = random.randint(0, 9)
+        if rand in answer:
+            pass
+        else:
+            answer += [rand]
+    return answer
+
 def main():
     #答えの生成
     answer = random.randint(100, 999)
 
-    guess()
-
-def guess():
     max_count = 10
     for i in range(1, max_count + 1):
         print('３桁の数字を入力してください')
         num = int(input())
+        if num == answer:
+            print('正解です！おめでとうございます！')
+            break
+
+
 
 
 if __name__ == '__main__':
